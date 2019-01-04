73aaf05c2361dc0c59fc0f55729fb5f3293856a5
Fachri Riyanto <fachririyanto.20@gmail.com>
2019-01-04 09:27:41 +0700

    Update description.

--- settings/clients/starter.js
+++ settings/clients/starter.js
@@ -5,7 +5,7 @@ module.exports = {
         version: "1.0.0",
         author: {
             name: "Fachri Riyanto",
-            url: "https://fachririyanto@gmail.com",
+            url: "https://fachririyanto.com",
             email: "fachririyanto@gmail.com"
         }
     },
